2e54a686d7c5043f6b63907b3a3b1beac8831eb5
Rotem Mizrah <rotem7353@gmail.com>
2023-05-16 12:21:28 +0400

    Added Excesrise Two

--- Excercise_1/index.js
+++ Excercise_1/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+console.log("Hello World");
+console.log(typeof numberOfStudents)
+var numberOfStudents = 25;
+console.log(typeof numberOfStudents)
+numberOfStudents = 99;
+console.log(typeof (typeof numberOfStudents))
+numberOfStudents = "fd";
+console.log(typeof numberOfStudents)
---
+++ Excierce_2/index.js
@@ -0,0 +1,15 @@
+var firstName = "Rotem";
+var age = 25;
+
+console.log(firstName, age);
+
+console.log(firstName.lastIndexOf("R"));
+
+console.log(firstName.charAt(0));
+
+var longString = "Thgis is bery long string";
+var isEndsWith = longString.endsWith("long string");
+
+console.log("IsEndsWith: ", isEndsWith)
+
+console.log("String lenght", firstName)
