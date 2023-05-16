7182268b08efcf88ff33844e441581c1a8368d26
Rotem Mizrah <rotem7353@gmail.com>
2023-05-16 16:06:52 +0400

    Added excirse 3

--- Excierce_2/index.js
+++ Excierce_2/index.js
@@ -13,3 +13,26 @@ var isEndsWith = longString.endsWith("long string");
 console.log("IsEndsWith: ", isEndsWith)
 
 console.log("String lenght", firstName)
+
+
+// Excerise 
+// In case the evalued value is Nan, print 'i ccont proceed'
+
+// In case the evalued value is number, print the number multiplied by 3
+
+// In case the evalued value is string, print the last charcted of the string V
+
+// If nothig from the above, print the evaluted value
+
+var toBeEvaluated = "\" hello world!! \" ";
+
+var evalutedValue = eval(toBeEvaluated);
+console.log(evalutedValue, typeof evalutedValue);
+
+if( typeof evalutedValue == "string" ){
+    var lastIndex = evalutedValue.length - 1;
+    console.log(evalutedValue.charAt(lastIndex))
+} else {
+    var valueToPrint = isNaN(evalutedValue) ? "I cannot proceed" : (typeof evalutedValue === "number") ? evalutedValue * 3 : evalutedValue; 
+    console.log(valueToPrint)
+}
