daeecdc5cc42b60d50fa4bb432be9831fb2b64d7
Rotem Mizrah <rotem7353@gmail.com>
2023-05-19 11:43:27 +0400

    Added functions comments

--- Game/index.js
+++ Game/index.js
@@ -17,17 +17,70 @@ var rooms = {
         description: "You are in a dark room... You have hallway to the north and a door to the east",
         exits: {
             north: "hallway"
-        }
+        },
+        items: []
     },
     hallway: {
         description: "You are in scary hallway... there is zombies behind you, and door to the east",
         exits: {
             back: "start",
             behind: "zombies",
-            east: "door"
+            east: "door locked"
+        },
+        items: ['key']
+    },
+    "door locked": {
+        description: "You are in fornt of locked door. there is a keyhole",
+        exits: {
+            // east: "treasure room" -> Should be exist onlt if the key is used
         }
+    },
+    "treasure room": {
+        description: "You are in the treasure room, look for it",
+        items: ['treasure']
     }
+
 }
+
+function getCurrentRoomObject(){
+    var currentLocation = player.currentLocation;
+    return rooms[currentLocation];
+}
+
+
+function use(item){
+
+    // check if the user have the item
+    // if the player is located in the locked room and the item is key
+    // we will do the following:
+    // 1. key shouled be removed fomr the inventory
+    // 2. we will add new exist to the locked door room
+
+}
+
+
+/**
+ * Responsible for taking an item from the current room
+ * and put it into the users inventory.
+ * The item should be taken only if exist in the room.
+ * The item should be removed from the room after it has been taken.
+ * @param {string} item 
+ */
+function take(item){
+    var roomObject = getCurrentRoomObject();
+    var roomItems = roomObject.items;
+
+    if(!roomItems.includes(item)){
+        return console.log("This item is not located in ypur room...")
+    }
+
+    var itemIndex = roomItems.indexOf(item)
+    player.invetory.push(item);
+    roomItems.splice(itemIndex, 1);
+
+    return console.log("We took the item!");
+}
+
 
 /**
  * Go function should allow the user to move from one room to another.
@@ -35,8 +88,7 @@ var rooms = {
  * @param {string} direction 
  */
 function go(direction){
-    var currentLocation = player.currentLocation;
-    var roomObject = rooms[currentLocation];
+    var roomObject = getCurrentRoomObject()
     var roomExits = roomObject.exits;
 
     if(!roomExits[direction]){
@@ -73,8 +125,11 @@ function parseCommand(command){
         case "go":
             go(objective);
             break;
+        case 'take':
+            take(objective);
+            break;
         default:
-            console.log('this action is...');
+            console.log('this action is no supprted...');
             break;
     }
 
@@ -99,5 +154,10 @@ while(player.health > 0){
 
     var userCommand = prompt("What do you want to do?");
     parseCommand(userCommand);
+
+    if(player.invetory.includes('treasure')){
+        console.log("You win!!!!!!!!")
+        break;
+    }
 
 }
